869cd0d663b0dcf6ab4733c5ea7ece1cf72e6a0b
Shahrozirl <shahrozirl@gmail.com>
2025-11-05 23:33:26 +0000

    added a constrast button due to the fact some people prefer/find it easier to read when backround is black and words are white

--- javascript/script.js
+++ javascript/script.js
@@ -10,3 +10,18 @@ function changeTextSize(direction) {
   currentSize += direction * 10; // increase or decrease by 10%
   document.body.style.fontSize = currentSize + "%";
 }
+
+function toggleContrast() {
+  const body = document.body;
+  const btn = document.getElementById("contrastToggle");
+
+  body.classList.toggle("high-contrast");
+
+  // Change icon depending on mode
+  if (body.classList.contains("high-contrast")) {
+    btn.textContent = "🌙"; // moon icon
+  } else {
+    btn.textContent = "☀️"; // sun icon
+  }
+}
+
